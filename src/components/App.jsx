72ac35cc7b765aa0375dcd3e62d8a2b60d6c9e49
Rafał Gałecki <darthunter@gmail.com>
2023-03-19 11:41:59 +0100

    Filter works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactsList from './ContactsList/ContactsList';
 import ContactsListElement from './ContactslistElement/ContactsListElement';
+import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
@@ -19,12 +20,27 @@ export class App extends Component {
     });
     console.log('CONTACTS:', this.state.contacts);
   };
+
   deleteContact = id => {
     this.setState({
       contacts: this.state.contacts.filter(contact => contact.id !== id),
     });
   };
+
+  filter = element => {
+    this.setState({
+      filter: element.currentTarget.value,
+    });
+  };
+
+  filterContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact => contact.name.includes(filter));
+  };
+
   render() {
+    const value = this.state.filter;
+    const filteredContacts = this.filterContacts();
     return (
       <div>
         <h1>Phonebook</h1>
@@ -34,9 +50,10 @@ export class App extends Component {
           // onSubmit={values => console.log('VALUES:', values)}
         />
         <h2>Contacts</h2>
+        <Filter value={value} filterContacts={this.filterContacts} onChange={this.filter } />
         <ContactsList>
           <ContactsListElement
-            contacts={this.state.contacts}
+            contacts={filteredContacts}
             deleteContact={this.deleteContact}
           />
         </ContactsList>
